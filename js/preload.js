aa7d929df296466c416f40d91a50223dabc4f94b
Tsuki <i@sukitsuki.com>
2016-06-12 12:50:42 +0800

    Site updated: 2016-06-12 12:50:42

--- js/preload.js
+++ js/preload.js
@@ -2,15 +2,6 @@
  * Created by Alicex on 6/11/16.
  */
 (function ($) {
-  var setTagsColor = function () {
-    var tags = $(".tagcloud a");
-    for (var i = 0; i < tags.length; i++) {
-      var num = Math.floor(Math.random() * 7);
-      tags.eq(i).addClass("color" + num);
-    }
-    $(".article-category a:nth-child(-n+2)").attr("class", "color0");
-  };
-  setTagsColor();
   if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
     return
   }
